2a2d7aa278c7f5724978a67bb12581c977c3e1c2
victor <victor.gaudiot@gmail.com>
2019-11-27 00:33:15 -0300

    servidor finalizado...eu acho

--- serverUI.java
+++ serverUI.java
@@ -1,6 +1,28 @@
+import javax.swing.*;
+
 public class serverUI {
 
-    public serverUI(){
 
+    private JPanel panel;
+    private JTextArea textArea;
+    private JFrame janela;
+    private servidor server;
+
+    public serverUI(servidor server){
+        this.server = server;
+
+        janela = new JFrame();
+        janela.setContentPane(panel);
+        janela.setVisible(true);
+        janela.pack();
+        janela.setSize(300, 100);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void updateStatus(){
+        if(server.isOnline1) textArea.setText(server.id1 + " está online\n");
+        else textArea.setText(server.id1 + " está offline\n");
+        if(server.isOnline2) textArea.append(server.id2 + " está online\n");
+        else textArea.append(server.id2 + " está offline\n");
     }
 }
--- servidor.java
+++ servidor.java
@@ -6,6 +6,22 @@ import java.nio.ByteBuffer;
 
 public class servidor {
 
+	//Informacoes dos clientes
+	public String id1, id2;
+	public boolean isOnline1, isOnline2;
+	public int portMsg1 = -1, portMsg2 = -1;
+	public int portAudio1 = -1, portAudio2 = -1;
+	public InetAddress ip1 = null, ip2 = null;
+
+	public servidor(){
+		//Informacoes dos clientes
+		this.id1 = "Cliente 1"; this.id2 = "Cliente 2";
+		this.isOnline1 = false; this.isOnline2 = false;
+		this.portMsg1 = -1; this.portMsg2 = -1;
+		this.portAudio1 = -1; this.portAudio2 = -1;
+		this.ip1 = null; this.ip2 = null;
+	}
+
 	public static byte[] concatenarBytes(byte[] bytes1, byte[] bytes2){
 		byte[] new_byte = new byte[bytes1.length + bytes2.length];
 		int k = 0;
@@ -29,6 +45,11 @@ public class servidor {
 	public static void main(String[] args){
 		//Porta usada pelo servidor
 		int serverPort = 9999;
+		servidor server = new servidor();
+
+		//Abrindo a interface do servidor
+		serverUI sUI = new serverUI(server);
+		sUI.updateStatus();
 
 		try {
 			DatagramSocket serverSocket = new DatagramSocket(serverPort);
@@ -36,76 +57,66 @@ public class servidor {
 			
 			byte[] receiveData = new byte[1024];
 			byte[] sendData;
-
-			//Informacoes dos clientes
-			String id1 = null, id2 = null;
-			boolean isOnline1 = false, isOnline2 = false;
-            int portMsg1 = -1, portMsg2 = -1;
-			int portAudio1 = -1, portAudio2 = -1;
-			InetAddress ip1 = null, ip2 = null;
-
-			//Abrindo a interface do servidor
-			serverUI sUI = new serverUI();
 			
 			while(true) {
 				receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				serverSocket.receive(receivePacket);
 				DatagramPacket sendPacket;
 
-				if(ip1 == null) {
+				if(server.ip1 == null) {
 
 					//Seta os valores para a porta e enderecoIP do cliente1
-					portMsg1 = receivePacket.getPort();
-					ip1 = receivePacket.getAddress();
-					isOnline1 = true;
+					server.portMsg1 = receivePacket.getPort();
+					server.ip1 = receivePacket.getAddress();
+					server.isOnline1 = true;
 					ByteBuffer wrapped = ByteBuffer.wrap(receivePacket.getData(), 0, 4);
-					portAudio1 = wrapped.getInt();
-					id1 = new String(receivePacket.getData(), 4, receivePacket.getLength()-4);
+					server.portAudio1 = wrapped.getInt();
+					server.id1 = new String(receivePacket.getData(), 4, receivePacket.getLength()-4);
 					//System.out.print(id1 + " " + portAudio1);
 					System.out.println("Hello");
 
-				}else if(ip1.equals(receivePacket.getAddress())){
+				}else if(server.ip1.equals(receivePacket.getAddress()) && server.portMsg1 == receivePacket.getPort()){
 					//Envia uma mensagem ao cliente 2 sobre o novo estado do cliente 1
-					isOnline1 = !isOnline1;
-					sendData = boolToByte(isOnline1);
-					sendPacket = new DatagramPacket(sendData, sendData.length, ip2, portMsg2);
+					server.isOnline1 = !server.isOnline1;
+					sendData = boolToByte(server.isOnline1);
+					sendPacket = new DatagramPacket(sendData, sendData.length, server.ip2, server.portMsg2);
 					serverSocket.send(sendPacket);
-					System.out.println(isOnline1);
 
-				}else if(ip2 == null) {
+				}else if(server.ip2 == null) {
 
 					//Seta os valores para a porta e enderecoIP do cliente2
-					portMsg2 = receivePacket.getPort();
-					ip2 = receivePacket.getAddress();
-					isOnline2 = true;
+					server.portMsg2 = receivePacket.getPort();
+					server.ip2 = receivePacket.getAddress();
+					server.isOnline2 = true;
 					ByteBuffer wrapped = ByteBuffer.wrap(receivePacket.getData(), 0, 4);
-					portAudio2 = wrapped.getInt();
-					id2 = new String(receivePacket.getData(), 4, receivePacket.getLength()-4);
+					server.portAudio2 = wrapped.getInt();
+					server.id2 = new String(receivePacket.getData(), 4, receivePacket.getLength()-4);
 					//System.out.print(id2 + " " + portAudio2);
 
 					//Envia ao cliente2 as informacoes do cliente1
-					sendData = concatenarBytes(ip1.getAddress(), intToByte(portMsg1));
-					sendData = concatenarBytes(sendData, intToByte(portAudio1));
-					sendData = concatenarBytes(sendData, id1.getBytes());
-					sendPacket = new DatagramPacket(sendData, sendData.length, ip2, portMsg2);
+					sendData = concatenarBytes(server.ip1.getAddress(), intToByte(server.portMsg1));
+					sendData = concatenarBytes(sendData, intToByte(server.portAudio1));
+					sendData = concatenarBytes(sendData, server.id1.getBytes());
+					sendPacket = new DatagramPacket(sendData, sendData.length, server.ip2, server.portMsg2);
 					serverSocket.send(sendPacket);
 
 					//Envia ao cliente1 as informacoes do cliente2
-					sendData = concatenarBytes(ip2.getAddress(), intToByte(portMsg2));
-					sendData = concatenarBytes(sendData, intToByte(portAudio2));
-					sendData = concatenarBytes(sendData, id2.getBytes());
-					sendPacket = new DatagramPacket(sendData, sendData.length, ip1, portMsg1);
+					sendData = concatenarBytes(server.ip2.getAddress(), intToByte(server.portMsg2));
+					sendData = concatenarBytes(sendData, intToByte(server.portAudio2));
+					sendData = concatenarBytes(sendData, server.id2.getBytes());
+					sendPacket = new DatagramPacket(sendData, sendData.length, server.ip1, server.portMsg1);
 					serverSocket.send(sendPacket);
 
 					System.out.println("World");
-				}else if(ip2.equals(receivePacket.getAddress())){
+				}else if(server.ip2.equals(receivePacket.getAddress()) && server.portMsg2 == receivePacket.getPort()){
 					//Envia uma mensagem ao cliente 1 sobre o novo estado do cliente 2
-					isOnline2 = !isOnline2;
-					sendData = boolToByte(isOnline2);
-					sendPacket = new DatagramPacket(sendData, sendData.length, ip1, portMsg1);
+					server.isOnline2 = !server.isOnline2;
+					sendData = boolToByte(server.isOnline2);
+					sendPacket = new DatagramPacket(sendData, sendData.length, server.ip1, server.portMsg1);
 					serverSocket.send(sendPacket);
-					System.out.println(isOnline2);
 				}
+
+				sUI.updateStatus();
 			}
 		} catch (IOException e) {
 			System.out.println("Problema");
